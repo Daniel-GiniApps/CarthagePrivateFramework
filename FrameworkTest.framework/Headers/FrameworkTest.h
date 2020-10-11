756a8811007d000ed61ea1df21a29f4778144887
Daniel Broyde <danielbroyde.work@gmail.com>
2020-10-11 22:40:27 +0300

    Framework

---
+++ FrameworkTest.framework/Headers/FrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by Daniel on 08/10/2020.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+
